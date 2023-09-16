7cd35b973079c2f49dfdd688e19f1bd23ed9a047
unknown <ha.kradol@ksr.ch>
2023-09-16 21:00:32 +0200

    birth of new cells work

--- game_of_life.py
+++ game_of_life.py
@@ -1,24 +1,7 @@
 
 import random
 import numpy as np
-
-# Create a 3x4 array filled with zeros
-a = random.randint(0,3)
-
-# my_array = np.zeros((3, 4)) #3 is lentgh y and 4 lenth x
-# my_arra = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-#print(my_arra[0,1]) this prints number 2
-
-# my_arra = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# print(my_arra[0,1])
-# my_arra[0,1] = 10
-# print(my_arra[0,1])
-
-
-# for row in my_arra:
-#     print(row)
-#     for element in row:
-#         print(element)
+import time
 
 def create_starting_map(x_len,y_len, num_alive): 
     mapp = np.zeros((y_len, x_len))
@@ -36,18 +19,17 @@ def create_starting_map(x_len,y_len, num_alive):
             mapp[y,x] = 1
             placed_ones += 1   
             pos_alive.append([y,x])
-
     return mapp, pos_alive
 
-mappe, position = create_starting_map(7,9, 40)
-print(mappe, position)
 
-def live_die(mapp, alive_pos):
+
+def live_die_born(mapp, alive_pos):
     y = mapp.shape[0] #gives lenght y
     x = mapp.shape[1] #gives lengh x
-    print(mapp, alive_pos)
-    next_mapp = mapp
+
+    next_map = mapp
     still_alive = []
+    chance_realiven = []
 
     for element in alive_pos:
         alive_neighbours = 0
@@ -56,57 +38,53 @@ def live_die(mapp, alive_pos):
 
         for e in range(3):
             for i in range(3):
+                
                 if element_top_corner in alive_pos and element_top_corner != element:
                     alive_neighbours += 1
-                    #print("yay")
+
+                if element_top_corner not in alive_pos: #and 0<=element_top_corner[0]<=(y-1) and 0<=element_top_corner[1]<=(x-1):
+                    if element_top_corner[0] >= 0 and element_top_corner[1] >= 0:
+                        if element_top_corner[0] < y and element_top_corner[1] <x:       
+                            test = element_top_corner.copy()
+                            chance_realiven.append(test)
+
                 element_top_corner[1] += 1
+
             element_top_corner[0] += 1
             element_top_corner[1] -= (x-1)
 
-        print(alive_neighbours,element)
-
         if alive_neighbours < 2:
-            next_mapp[element[0],element[1]] = 0
-
-            print("die")
+            next_map[element[0],element[1]] = 0
 
         if alive_neighbours == 3 or alive_neighbours == 2:
             still_alive.append(element)
-            print("stay alive")
 
         if alive_neighbours > 3:
-            next_mapp[element[0],element[1]] = 0
-            print("die")
+            next_map[element[0],element[1]] = 0
 
-    print(next_mapp, still_alive)
+    for element in chance_realiven:
+        count = chance_realiven.count(element)
+        if count ==3 and next_map[element[0], element[1]] == 0:
+            still_alive.append(element)
+            next_map[element[0],element[1]] = 1
+    
+    return next_map, still_alive
 
-live_die(mappe, position)
 
+start_map, position = create_starting_map(5,7, 20)
+run = True
+print("first generation:")
+print(start_map)
 
+while run:
+    next_gen, alive_coord = live_die_born(start_map, position)
 
+    start_map = next_gen
+    position = alive_coord
+    print("next generation:")
+    print(start_map)
+    time.sleep(5)
 
-
-# list = [[2, 3], [1, 3], [0, 3], [2, 1], [3, 1]]         
-
-# for element in list:
-#     alive_neighbours = 0
-#     element_top_corner = [element[0]-1, element[1]-1]
-#     for e in range(3):
-#         for i in range(3):
-#             if element_top_corner in list:
-#                 alive_neighbours += 1
-#                 print("yay")
-#             element_top_corner[1] += 1
-#         element_top_corner[0] += 1
-#         element_top_corner[1] -= 3
-
-#     if alive_neighbours < 2:
-#         print("die")
-
-#     elif alive_neighbours == 3:
-#         print("stay alive")
-#     elif alive_neighbours > 3:
-#         print("die")
 
             
 
